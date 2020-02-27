06f28752f3ad61205b0d21c6c659495e88e72954
Reagan Wood <reaganwood1@gmail.com>
2020-02-27 08:45:59 -0600

    added rest framework

--- peakapi/peakapi/urls.py
+++ peakapi/peakapi/urls.py
@@ -14,19 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from quickstart import views
 
 from pages.views import home_view, contact_view
 from products.views import product_detail_view, product_create_view, render_initial_form, dynamic_lookup_view, product_delete_view, product_list_view
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# urlpatterns = [
+# 	path('', home_view, name='home'),
+# 	path('contact/', contact_view),
+#     path('product/', product_detail_view),
+#     path('product/<int:id>/', dynamic_lookup_view, name='product'),
+#     path('product/<int:id>/delete/', product_delete_view, name='product-delete'),
+#     path('products/', product_list_view, name='product-list'),
+#     path('create/', product_create_view),
+#     path('admin/', admin.site.urls),
+#     path('initial/', render_initial_form),
+# ]
+
 urlpatterns = [
-	path('', home_view, name='home'),
-	path('contact/', contact_view),
-    path('product/', product_detail_view),
-    path('product/<int:id>/', dynamic_lookup_view, name='product'),
-    path('product/<int:id>/delete/', product_delete_view, name='product-delete'),
-    path('products/', product_list_view, name='product-list'),
-    path('create/', product_create_view),
-    path('admin/', admin.site.urls),
-    path('initial/', render_initial_form),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
