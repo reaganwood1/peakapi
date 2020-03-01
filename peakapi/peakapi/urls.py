3a7a5b2b16d47e03280a045c650d5971589d6aa2
Reagan Wood <reaganwood1@gmail.com>
2020-03-01 11:44:52 -0600

    add a sample route with getting authenticated data

--- peakapi/peakapi/urls.py
+++ peakapi/peakapi/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import login
+from .views import login, sample_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login),
+    path('sampleapi/', sample_api),
 ]
--- peakapi/peakapi/views.py
+++ peakapi/peakapi/views.py
@@ -27,3 +27,11 @@ def login(request):
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key},
                     status=HTTP_200_OK)
+
+
+
+@csrf_exempt
+@api_view(["GET"])
+def sample_api(request):
+    data = {'sample_data': 123}
+    return Response(data, status=HTTP_200_OK)
