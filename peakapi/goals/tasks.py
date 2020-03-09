79b5aae85f8345e3af392cd7b3458ae91ea7d10a
Reagan Wood <reaganwood1@gmail.com>
2020-03-08 21:27:21 -0500

    fixed why command wasn't working

--- peakapi/goals/tasks.py
+++ peakapi/goals/tasks.py
@@ -1,11 +1,12 @@
 from background_task import background
 from django.contrib.auth.models import User
 from goals.models import Goal, GoalChallenge, GoalAttempt, GoalAttemptEntry
+from datetime import datetime, timedelta
 
 # TODO: fix task scheduler
-@background(schedule=30)
+# @background(schedule=30)
 def add_failure_at_start_of_day():
-    goal_attempts = GoalAttempt.objects.filter(misess_remaining >= 0, completed=False)
+    goal_attempts = GoalAttempt.objects.filter(misess_remaining__gte=0, completed=False)
 
     for goal_attempt in goal_attempts:
     	clear_goal_attempt(goal_attempt)
@@ -22,10 +23,11 @@ def clear_goal_attempt(goalAttempt: GoalAttempt):
 	entrys = GoalAttemptEntry.objects.filter(goal_attempt=goalAttempt)
 
 	for entry in entrys:
+		print("TRYING THIS")
 		if entry.created_at.date() >= today:
 			return
 
 
 	print("SUBTRACTING")
 	goalAttempt.misess_remaining -= 1
-	goalAttempt.save()
+	# goalAttempt.save()
